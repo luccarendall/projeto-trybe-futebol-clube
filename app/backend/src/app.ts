306cccb33144febb700e122981f08793cac5bcde
Lucca Rendall <luccarendall2@gmail.com>
2022-11-03 13:01:54 -0300

    Update app.ts

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -10,7 +10,7 @@ class App {
     this.config();
 
     this.app.use('/login', Route.loginRoute);
-    this.app.use('/login', Route.teamsRoute);
+    this.app.use('/teams', Route.teamsRoute);
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
