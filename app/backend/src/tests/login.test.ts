bf2ac6846f6db1e40564edc5933030e2b031ee6d
Lucca Rendall <luccarendall2@gmail.com>
2022-11-02 23:38:46 -0300

    Testes login parte 1

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,79 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as JWT from 'jsonwebtoken';
+
+import chaiHttp from 'chai-http';
+
+import { app } from '../app';
+import User from '../database/models/UserModel';
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+const UserLogin = {
+  email: 'divinesmite@critic.com',
+  password: 'secret_admin',
+}
+
+const userSeeders =  [
+  {
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      // senha: secret_admin
+  },
+  {
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+      // senha: secret_user
+  },
+]
+
+const tokenLogin = JWT.sign({ userId: 1 }, 'jwt_secret', {
+  expiresIn: '1d',
+});
+
+describe('Rota Login', () => {
+  describe('Usuário válido', () => {
+    beforeEach(async () => {
+      sinon.stub(User, 'findOne').resolves(userSeeders[0] as User);
+    })
+
+    afterEach(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    })
+    it('Deve retornar o status 200 caso tenha sucesso no login', async () => {
+      const response = await chai.request(app)
+        .post('/login')
+        .send(UserLogin);
+
+      expect(response.body).to.have.property('token');
+      expect(response.status).to.be.equal(200);
+    });
+  })
+  describe('Testes de validações para a rota /Login', () => {
+    beforeEach(async () => {
+      sinon.stub(User, 'findOne').resolves(userSeeders[0] as User);
+    })
+
+    afterEach(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    })
+    it('Deve retornar a role do usuário encontrado', async () => {
+      const response = await chai.request(app)
+        .get('/login/validate')
+        .set('authorization', tokenLogin);
+        expect(response.body).to.be.deep.equal({ role: userSeeders[0].role });
+        expect(response.status).to.be.equal(200);
+    })
+
+    it("Deve retornar status 400, caso não tenha o header Authorization", async () => {
+      const response = await chai.request(app).get('/login/validate')
+      expect(response.status).to.equal(400)
+    })
+  })
+})
