71124e38378fbe82e4e32b54a81a7d2dc0b02386
Lucca Rendall <luccarendall2@gmail.com>
2022-11-03 02:46:53 -0300

    Mudando forma de importação do chaiHttp. O lint reclama mas se não coloco require o test:coverage quebra

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -2,7 +2,8 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 import * as JWT from 'jsonwebtoken';
 
-import chaiHttp from 'chai-http';
+// O lint indica que tá errado mas se coloco com o from o test:coverage quebra
+import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import User from '../database/models/UserModel';
@@ -12,22 +13,22 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 const userLogin = {
-  email: 'user@user.com',
-  password: 'secret_user',
+  email: 'lucca@admin.com',
+  password: 'secret_admin',
 }
 
 const userSeeders =  [
   {
-    username: 'Admin',
-    role: 'admin',
-    email: 'admin@admin.com',
+    username: 'luccarendall',
+    role: 'lucca',
+    email: 'lucca@admin.com',
     password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
       // senha: secret_admin
   },
   {
-    username: 'User',
-    role: 'user',
-    email: 'user@user.com',
+    username: 'lunamaria',
+    role: 'luna',
+    email: 'luna@maria.com',
     password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
       // senha: secret_user
   },
