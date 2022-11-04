464a2fa997c5cf86b58d725b937cfee4dc586c7b
Lucca Rendall <luccarendall2@gmail.com>
2022-11-04 04:01:30 -0300

    middleware de validação matches

---
+++ app/backend/src/middlewares/ValidateMatch.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import * as JWT from 'jsonwebtoken';
+
+import TokenInterface from '../interfaces/TokenInterface';
+import CustomError from '../CustomError/CustomError';
+
+// DOC: https://community.smartbear.com/t5/SwaggerHub-Questions/How-to-replace-quot-bearer-quot-in-Bearer-Authentication-with/td-p/207705
+
+const ValidateMatch = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+  const { homeTeam, awayTeam } = req.body;
+
+  if (homeTeam === awayTeam) {
+    return res.status(401).json({
+      message: 'It is not possible to create a match with two equal teams',
+    });
+  }
+
+  try {
+    if (!authorization) {
+      return res.status(401).json({ message: 'Invalid Token!' });
+    }
+
+    const valitationToken = authorization.replace('Bearer ', '');
+
+    JWT.verify(valitationToken, 'jwt_secret') as TokenInterface;
+  } catch (err) {
+    throw new CustomError(401, 'Token must be a valid token');
+  }
+
+  next();
+};
+
+export default ValidateMatch;
--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matchesController';
+import validateMatch from '../middlewares/ValidateMatch';
 
 const matchesRoute = Router();
 
@@ -7,7 +8,7 @@ const matchesController = new MatchesController();
 
 // matchesRoute.get('/', matchesController.findAllMatchesData);
 matchesRoute.get('/', matchesController.findMatchByProgress);
-matchesRoute.post('/', matchesController.featNewMatch);
+matchesRoute.post('/', validateMatch, matchesController.featNewMatch);
 matchesRoute.put('/:id/finish', matchesController.finishedMatch);
 matchesRoute.put('/:id', matchesController.updateMatch);
 
